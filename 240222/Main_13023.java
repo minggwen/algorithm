ab6a1712b049c3a43cb367f014c6647782e99161
minggwen <tlsalsrud252@naver.com>
2024-02-26 21:48:37 +0900

    그래프&구현

--- 240222/Main_13023.java
+++ 240222/Main_13023.java
@@ -1,53 +1,55 @@
+package Algorithm;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_13023 {
 
-	static int N,M,parents[];
-	static List<List<Integer>> arr;
+	static int N,M;
+	static List<List<Integer>> edges;
+	static boolean[] visited;
+	static boolean flag = false;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N=Integer.parseInt(st.nextToken());
-		M=Integer.parseInt(st.nextToken());
-		arr = new ArrayList<>();
-		for(int n=0;n<N;n++) {//arr 정점의 개수만큼 초기화
-			arr.add(new ArrayList<Integer>());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		edges = new ArrayList<>();
+		for(int n=0; n<N;n++) {
+			edges.add(new ArrayList<>());
 		}
-		
 		for(int m=0;m<M;m++) {
 			st = new StringTokenizer(br.readLine());
 			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			arr.get(from).add(to);
+			int to =  Integer.parseInt(st.nextToken());
+			edges.get(from).add(to);
+			edges.get(to).add(from);
 		}
-		System.out.println(bfs()==true?1:0);
-		System.out.println(arr.toString());
-	}
-	private static boolean bfs() {
-		boolean[] visited = new boolean[N];
-		Queue<Integer> que = new ArrayDeque<>();
-		que.offer(0);
-		visited[0]=true;
-		int cnt = 0;
-		while(!que.isEmpty()) {
-			System.out.println(que.toString());
-			int num = que.poll();
-			cnt++;
-			for(int i=0;i<arr.get(num).size();i++) {
-				int tmp = arr.get(num).get(i);
-				if(visited[tmp])continue;
-				que.offer(tmp);
-				
-			}
+		for(int n=0;n<N;n++) {
+			visited = new boolean[N];
+			visited[n]=true;
+			dfs(n,0);
+			if(flag) break;
+			visited[n] = false;
 		}
-		return cnt >=4 ? true:false;
+		System.out.println(flag?1:0);
 	}
-	
+	private static void dfs(int start,int cnt) {
+		if(cnt==4) {
+			flag = true;
+			return;
+		}
+		for(int i = 0; i<edges.get(start).size();i++) {
+			int num = edges.get(start).get(i);
+			if(visited[num])continue;
+			visited[num] = true;
+			dfs(num,cnt+1);
+			visited[num] = false;
+		}
+	}
 }
+
--- 240222/Solution_1238.java
+++ 240222/Solution_1238.java
@@ -1,65 +1,64 @@
+package Algorithm;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Solution_1238 {
 
-	static int MAX_IDX = 0;
-	static int MAX=0;
-	static List<Queue<Integer>> edges;
-	static boolean[] visited = new boolean[101];
+	static int L,start;
+	static List<List<Integer>> arr;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		StringBuilder sb = new StringBuilder();
-		for(int t=1; t<=1;t++) {
+		for(int t=1;t<=1;t++) {
 			sb.append("#"+t+" ");
 			st = new StringTokenizer(br.readLine());
-			int L = Integer.parseInt(st.nextToken());
-			int Start = Integer.parseInt(st.nextToken());
-			edges = new ArrayList<>();
-			for(int i=0; i<=100;i++) {
-				Queue<Integer> que = new PriorityQueue<>(new Comparator<Integer>() {
-
-					@Override
-					public int compare(Integer o1, Integer o2) {
-						// TODO Auto-generated method stub
-						return o1-o2;
-					}
-				});
-				edges.add(que);
+			L = Integer.parseInt(st.nextToken());
+			start = Integer.parseInt(st.nextToken());
+			arr = new ArrayList<>();
+			for(int i=0; i<=100; i++) {
+				arr.add(new ArrayList<>());
 			}
 			st = new StringTokenizer(br.readLine());
 			for(int l=0;l<L/2;l++) {
 				int from = Integer.parseInt(st.nextToken());
 				int to = Integer.parseInt(st.nextToken());
-				edges.get(from).offer(to);
+				arr.get(from).add(to);
 			}
-			
-			sb.append(MAX_IDX).append("\n");
+			sb.append(bfs(start));
+			sb.append("\n");
 		}
 		System.out.println(sb.toString());
 	}
-	private static void bfs(int start) {
-		Queue<Integer> queue = new ArrayDeque<Integer>();
-		queue.offer(start);
-		visited[start] = true;
-		while(!queue.isEmpty()) {
-			int num = queue.poll();
-			for(int i=0; i<edges.get(num).size();i++) {
-				int poll = edges.get(num).poll();
-				if(visited[poll])continue;
-				queue.offer(poll);
-				visited[poll] = true;
+	private static int bfs(int start) {
+		Queue<Integer> que = new ArrayDeque<>();
+		boolean[] visited = new boolean[101];
+		que.offer(start);
+		visited[start]=true;
+		int result = 0;
+		while(!que.isEmpty()) {
+			int size = que.size();
+			int maxIdx = 0;
+			for(;size>0;size--) {
+				int num = que.poll();
+				for(int i=0; i<arr.get(num).size();i++) {
+					int offer = arr.get(num).get(i);
+					if(visited[offer]) continue;
+					que.offer(offer);
+					visited[offer] = true;
+					maxIdx=offer>maxIdx?offer:maxIdx;
+				}
 			}
+			if(maxIdx!=0)result = maxIdx;
 		}
+		return result;
 	}
 
 }
